e4ab0ca6f08f6ecd52abef70d9161c64532359a6
Sheila <sheila@webcentriq.com>
2023-08-04 20:36:37 +0330

    feat: create a new instance of notifications model for each model and update the count by creating new instance of the target model(s)

--- admin_notification/signals.py
+++ admin_notification/signals.py
@@ -2,23 +2,18 @@ from django.db.models.signals import post_delete, post_save, pre_save
 from django.apps.registry import Apps
 from django.core.exceptions import ImproperlyConfigured
 from django.shortcuts import render
-from django.conf import settings
 from django.shortcuts import HttpResponse, redirect
 from django.apps import apps as django_apps
 from admin_notification.models import Notification
 from django.dispatch import receiver
-
-try:
-    model = django_apps.get_model(settings.NOTIFICATION_MODEL, require_ready=False)
-except ValueError:
-    raise ImproperlyConfigured(
-        "NOTIFICATION_MODEL must be of the form 'app_label.model_name'"
-    )
+from admin_notification.utils import Settings
 
 
-@receiver(post_save, sender=model)
+@receiver(post_save)
 def post_save_handler(sender, **kwargs):
-    if kwargs['created']:
-        notification = Notification.objects.all().first()
+    models = Settings.NOTIFICATION_MODELS
+    if kwargs["created"] and sender in models:
+        model = Settings.get_selected_model(sender)
+        notification = Notification.objects.get_or_create(model=model)
         notification.count += 1
         notification.save()
